042b400f8dadce5ac50fe217b111aa2294b71276
IlyaTikhanov-sandbox <dotahub@icloud.com>
2024-03-02 21:03:46 +0100

    SpriteComponent Refactoring: added m_ to private fields

--- CosmoSpaceShip/CosmoSpaceShip/Src/ECS/SpriteComponent.h
+++ CosmoSpaceShip/CosmoSpaceShip/Src/ECS/SpriteComponent.h
@@ -21,30 +21,30 @@ enum Anims : int
 };
 
 //TODO:
-//Make std::bitset<maxAnims> animsEnabled to work
-//every Play(Anim)should set animsEnabled bit of Anim to be played
+//Make std::bitset<maxAnims> m_animsEnabled to work
+//every Play(Anim)should set m_animsEnabled bit of Anim to be played
 //Sprite component should decide which one to play depending on priority
 //Ex. WasHit > Idle to
-//Challenge: When to disable Anim's bit from animsEnabled??
+//Challenge: When to disable Anim's bit from m_animsEnabled??
 
 
 class SpriteComponent : public Component
 {
 private:
 
-	int size = 0;
-	int frames = 0;
-	int speed = 100;
-	bool animated = false;
+	int m_size = 0;
+	int m_frames = 0;
+	int m_speed = 100;
+	bool m_animated = false;
 
-	SDL_Texture* texture;
-	SDL_Rect srcRect, destRect;
+	SDL_Texture* m_texture;
+	SDL_Rect m_srcRect, m_destRect;
 	
-	int animIndex = 0;
-	std::bitset<maxAnims> animsEnabled;
-	std::map<int, Animation> animations;
+	int m_animIndex = 0;
+	std::bitset<maxAnims> m_animsEnabled;
+	std::map<int, Animation> m_animations;
 	
-	SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
+	SDL_RendererFlip m_spriteFlip = SDL_FLIP_NONE;
 
 public:
 
@@ -57,7 +57,7 @@ public:
 
 	SpriteComponent(std::string id, bool isAnimated, int width = 32)
 	{
-		animated = isAnimated;
+		m_animated = isAnimated;
 
 		Animation idle = Animation(0, 4, 150);
 		Animation wasHit = Animation(1, 4, 150);
@@ -65,65 +65,65 @@ public:
 		Animation BossIdle = Animation(0, 8, 300);
 		Animation BossWasHit = Animation(1, 8, 300);
 
-		animations.emplace(Anims::Idle, idle);
-		animations.emplace(Anims::WasHit, wasHit);
-		animations.emplace(Anims::Dead, dead);
-		animations.emplace(Anims::BossIdle, BossIdle);
-		animations.emplace(Anims::BossWasHit, BossWasHit);
+		m_animations.emplace(Anims::Idle, idle);
+		m_animations.emplace(Anims::WasHit, wasHit);
+		m_animations.emplace(Anims::Dead, dead);
+		m_animations.emplace(Anims::BossIdle, BossIdle);
+		m_animations.emplace(Anims::BossWasHit, BossWasHit);
 
 		Play(Anims::Idle);
 		setTex(id);
-		size = width;
+		m_size = width;
 	}
 
 	~SpriteComponent() {}
 
 /// --- Getters
 
-	SDL_RendererFlip getSpriteFlip() { return spriteFlip; }
+	SDL_RendererFlip getSpriteFlip() { return m_spriteFlip; }
 
 /// --- Setters
 
-	void setSpriteFlip(SDL_RendererFlip _spriteFlip) { spriteFlip = _spriteFlip; }	
-	void setTex(std::string id) { texture = Game::assets->GetTexture(id); }
+	void setSpriteFlip(SDL_RendererFlip _spriteFlip) { m_spriteFlip = _spriteFlip; }	
+	void setTex(std::string id) { m_texture = Game::assets->GetTexture(id); }
 
 /// --- Core Component functions
 
 	void init() override
 	{
-		srcRect.x = srcRect.y = 0; //WA to draw Source Rect > 32x32
+		m_srcRect.x = m_srcRect.y = 0; //WA to draw Source Rect > 32x32
 		
-		srcRect.w = entity->getComponent<TransformComponent>().getWidth();
-		srcRect.h = entity->getComponent<TransformComponent>().getHeight();
+		m_srcRect.w = entity->getComponent<TransformComponent>().getWidth();
+		m_srcRect.h = entity->getComponent<TransformComponent>().getHeight();
 	}
 
 	void update() override
 	{
-		if (animated)
+		if (m_animated)
 		{
-			srcRect.x = srcRect.w * static_cast<int>((SDL_GetTicks() / speed) % frames);
+			m_srcRect.x = m_srcRect.w * static_cast<int>((SDL_GetTicks() / m_speed) % m_frames);
 		}
 
-		srcRect.y = animIndex * srcRect.h; // animIndex * transform->height
+		m_srcRect.y = m_animIndex * m_srcRect.h; // m_animIndex * transform->height
 
 		Vector2D position = entity->getComponent<TransformComponent>().getPosition();
 
-		destRect.x = static_cast<int>(position.x) - Game::camera.x;
-		destRect.y = static_cast<int>(position.y) - Game::camera.y;
-		destRect.w = entity->getComponent<TransformComponent>().getScaledWidth();
-		destRect.h = entity->getComponent<TransformComponent>().getScaledHeight();
+		m_destRect.x = static_cast<int>(position.x) - Game::camera.x;
+		m_destRect.y = static_cast<int>(position.y) - Game::camera.y;
+		m_destRect.w = entity->getComponent<TransformComponent>().getScaledWidth();
+		m_destRect.h = entity->getComponent<TransformComponent>().getScaledHeight();
 	}
 
 	void draw() override
 	{
-		TextureManager::Draw(texture, srcRect, destRect,spriteFlip);
+		TextureManager::Draw(m_texture, m_srcRect, m_destRect,m_spriteFlip);
 	}
 
 	void Play(int animName)
 	{
-		frames = animations[animName].frames;
-		animIndex = animations[animName].index;
-		speed = animations[animName].speed;
+		m_frames = m_animations[animName].frames;
+		m_animIndex = m_animations[animName].index;
+		m_speed = m_animations[animName].speed;
 	}
 
 /// --- 
